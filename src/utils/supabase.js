99abcf5a0798e6be4c424328a5f141e999da7b8b
Nagender kumar <kumarnagender811@gmail.com>
2025-01-06 08:33:18 +0530

    refactor Supabase client setup to support authorization with access token

--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -1,7 +1,18 @@
-import { createClient } from "@supabase/supabase-js";
+import { createClient, SupabaseClient } from "@supabase/supabase-js";
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
-const supabase = createClient(supabaseUrl, supabaseKey);
 
-export default supabase;
+const SupabaseClient = async (supabaseAcessToken) => {
+  const supabase = createClient(supabaseUrl, supabaseKey, {
+    global: {
+      headers: {
+        Authorization: `Bearer ${supabaseAcessToken}`,
+      },
+    },
+  });
+
+  return supabase;
+};
+
+export default SupabaseClient;
