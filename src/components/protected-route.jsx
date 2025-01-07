647992d0121d7070e866e00329b6eb4bc9b6492f
Nagender kumar <kumarnagender811@gmail.com>
2025-01-07 14:11:48 +0530

    add ProtectedRoute component for user authentication handling

---
+++ src/components/protected-route.jsx
@@ -0,0 +1,17 @@
+import { useUser } from "@clerk/clerk-react";
+import { Navigate, useLocation } from "react-router-dom";
+
+const ProtectedRoute = ({children}) => {
+  const { isSignedIn, user, isLoaded } = useUser();
+  const { pathName } = useLocation();
+
+  if (isLoaded && !isSignedIn && isSignedIn !== undefined) {
+    return <Navigate to="/?sign-in=true" />;
+  }
+
+  // check onboarding status
+
+  return children;
+};
+
+export default ProtectedRoute;
