7d0b29014c5d02a4be4d28c1a7dfbc0682e63cdb
Nagender kumar <kumarnagender811@gmail.com>
2025-01-07 15:06:36 +0530

    enhance ProtectedRoute to redirect users based on onboarding status and role

--- src/components/protected-route.jsx
+++ src/components/protected-route.jsx
@@ -1,15 +1,21 @@
 import { useUser } from "@clerk/clerk-react";
 import { Navigate, useLocation } from "react-router-dom";
 
-const ProtectedRoute = ({children}) => {
+const ProtectedRoute = ({ children }) => {
   const { isSignedIn, user, isLoaded } = useUser();
-  const { pathName } = useLocation();
+  const { pathname } = useLocation();
 
   if (isLoaded && !isSignedIn && isSignedIn !== undefined) {
     return <Navigate to="/?sign-in=true" />;
   }
 
   // check onboarding status
+  if (
+    user !== undefined &&
+    !user?.unsafeMetadata.role &&
+    pathname !== "/onboarding"
+  )
+    return <Navigate to="/onboarding" />;
 
   return children;
 };
