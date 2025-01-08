b817d307cbfebea13ee8bb902bb384cbbe9b1084
Nagender kumar <kumarnagender811@gmail.com>
2025-01-08 23:02:47 +0530

    refactor supabase client function name for consistency

--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -1,9 +1,9 @@
-import { createClient, SupabaseClient } from "@supabase/supabase-js";
+import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-const SupabaseClient = async (supabaseAcessToken) => {
+const supabaseClient = async (supabaseAcessToken) => {
   const supabase = createClient(supabaseUrl, supabaseKey, {
     global: {
       headers: {
@@ -15,4 +15,4 @@ const SupabaseClient = async (supabaseAcessToken) => {
   return supabase;
 };
 
-export default SupabaseClient;
+export default supabaseClient;
