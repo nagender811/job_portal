69803c9b6a81381c620a87678eac92ace4463e21
Nagender kumar <kumarnagender811@gmail.com>
2025-01-08 23:03:18 +0530

    add job listing page with job fetching and loading states

--- src/pages/job-listing.jsx
+++ src/pages/job-listing.jsx
@@ -1,9 +1,61 @@
-import React from 'react'
+import { getJobs } from "@/api/apijobs";
+import JobCard from "@/components/job-card";
+import useFetch from "@/hooks/use-fetch";
+import { useUser } from "@clerk/clerk-react";
+import React, { useEffect, useState } from "react";
+import { BarLoader } from "react-spinners";
 
 const JobListing = () => {
-  return (
-    <div>JobListing</div>
-  )
-}
+  const [searchQuery, setSearchQuery] = useState("");
+  const [location, setLocation] = useState("");
+  const [company_id, setCompany_id] = useState("");
+  const { isLoaded } = useUser();
 
-export default JobListing
+  const {
+    fn: fnJobs,
+    data: jobs,
+    loading: loadingJobs,
+  } = useFetch(getJobs, {
+    searchQuery,
+    location,
+    company_id,
+  });
+
+  console.log(jobs);
+
+  useEffect(() => {
+    if (isLoaded) fnJobs();
+  }, [isLoaded, searchQuery, location, company_id]);
+
+  if (!isLoaded) {
+    return <BarLoader className="mb-4" width={"100%"} color="#36d7b7" />;
+  }
+
+  return (
+    <div>
+      <h1 className="gradient-title font-extrabold text-6xl sm:text-7xl text-center pb-8">
+        Latest Jobs
+      </h1>
+
+      {/* Add filters here */}
+
+      {loadingJobs && (
+        <BarLoader className="mt-4" width={"100%"} color="#36d7b7" />
+      )}
+
+      {loadingJobs === false && (
+        <div>
+          {jobs?.length ? (
+            jobs.map((job) => {
+              return <JobCard key={job.id} job={job} />
+            })
+          ) : (
+            <div>No Jobs Found😢</div>
+          )}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default JobListing;
