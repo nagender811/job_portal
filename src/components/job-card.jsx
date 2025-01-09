454d3f1b772b93833ca3c013b19602a4553f34d4
Nagender kumar <kumarnagender811@gmail.com>
2025-01-09 20:54:21 +0530

    feat: enhance JobCard component with save functionality and improved layout

--- src/components/job-card.jsx
+++ src/components/job-card.jsx
@@ -1,7 +1,9 @@
 import React from "react";
-import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "./ui/card";
 import { useUser } from "@clerk/clerk-react";
-import { MapIcon, Trash2Icon } from "lucide-react";
+import { Heart, MapPinIcon, Trash2Icon } from "lucide-react";
+import { Button } from "./ui/button";
+import { Link } from "react-router-dom";
 
 const JobCard = ({
   job,
@@ -9,7 +11,17 @@ const JobCard = ({
   isSavedInit = false,
   onJobSaved = () => {},
 }) => {
+
+  const {
+    fn: fnSavedjob,
+    data: savedjob,
+    loading: loadingSavedjob,
+  } = useFetch(savejob); 
+
   const { user } = useUser();
+
+  
+  
   return (
     <Card>
       <CardHeader>
@@ -25,14 +37,25 @@ const JobCard = ({
         </CardTitle>
       </CardHeader>
 
-      <CardContent>
-        <div>
+      <CardContent className="flex flex-col gap-4 flex-1">
+        <div className="flex justify-between">
             {job.company && <img src={job.company.logo_url} className="h-6"/>}
-            <div>
-                <MapIcon size={15}/> {job.location}
+            <div className="flex gap-2 items-center">
+                <MapPinIcon size={15}/> {job.location}
             </div>
         </div>
+        <hr />
+        {job.description.substring(0, job.description.indexOf("."))}
       </CardContent>
+      <CardFooter className="flex gap-2">
+        <Link to={`/job/${job.id}`} className="flex-1">
+          <Button variant="secondary" className="w-full">
+            More Details
+          </Button>
+        </Link>
+
+        <Heart size={20} stroke="red" fill="red"/>
+      </CardFooter>
     </Card>
   );
 };
