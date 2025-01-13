4662f2adabcbdb701c3c42c8bdbe90e506a70df4
Nagender kumar <kumarnagender811@gmail.com>
2025-01-13 18:01:18 +0530

    feat: add functionality to upload company logo and insert new company into the database

--- src/api/apiCompanies.js
+++ src/api/apiCompanies.js
@@ -1,14 +1,49 @@
-import supabaseClient from "@/utils/supabase";
+import supabaseClient, { supabaseUrl } from "@/utils/supabase";
 
 export async function getCompanies(token) {
-    const supabase = await supabaseClient(token)
+  const supabase = await supabaseClient(token);
 
-    const { data, error } = await supabase.from("companies").select("*")
+  const { data, error } = await supabase.from("companies").select("*");
 
-    if(error) {
-        console.error("Error Fetching Companies:", error);
-        return null;
-    }
+  if (error) {
+    console.error("Error Fetching Companies:", error);
+    return null;
+  }
 
-    return data;
+  return data;
+}
+
+export async function addNewCompany(token, _, companyData) {
+  const supabase = await supabaseClient(token);
+
+  const random = Math.floor(Math.random() * 90000);
+  const fileName = `logo-${random}-${companyData.name}`;
+
+  const { error: storageError } = await supabase.storage
+    .from("company-logo")
+    .upload(fileName, companyData.logo);
+
+  if (storageError) {
+    console.error("Error Uploading Company Logo:", storageError);
+    return null;
+  }
+
+  const logo_url = `${supabaseUrl}/storage/v1/object/public/company-logo"/${fileName}`;
+
+  const { data, error } = await supabase
+    .from("companies")
+    .insert([
+      {
+        name: companyData.name,
+        logo_url,
+      },
+    ])
+    .select();
+
+  if (error) {
+    console.error("Error Submiting Company:", error);
+    return null;
+  }
+
+  return data;
 }
